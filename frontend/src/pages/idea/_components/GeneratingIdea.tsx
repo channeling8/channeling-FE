fe83919bc80e242809de0d78ea773454419e9c3c
yoonvinjeong <anaabc1004@naver.com>
2025-10-31 18:53:38 +0900

    feat: 드롭다운 박스 기능 추가

--- frontend/src/pages/idea/_components/GeneratingIdea.tsx
+++ frontend/src/pages/idea/_components/GeneratingIdea.tsx
@@ -10,6 +10,7 @@ export const GeneratingIdea = () => {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false)
     const [keyword, setKeyword] = useState('')
     const [additionalInfo, setAdditionalInfo] = useState('')
+    const [selectedOption, setSelectedOption] = useState('')
 
     const handleDropdownClick = () => {
         setIsDropdownOpen((prev) => !prev)
@@ -19,6 +20,11 @@ export const GeneratingIdea = () => {
         setIsTooltipOpen((prev) => !prev)
     }
 
+    const handleOptionClick = (option: string) => {
+        setSelectedOption(option)
+        setIsDropdownOpen(true)
+    }
+
     return (
         <>
             <div className="flex justify-center items-center gap-2 relative">
@@ -26,7 +32,7 @@ export const GeneratingIdea = () => {
                 <Info className="justify-start" onClick={handleClick} />
                 {isTooltipOpen && <Ideatooltip className="absolute -right-[357px] -bottom-[19px]" />}
             </div>
-            <div className="flex flex-col w-[1200px] p-[16px] items-start gap-[16px] rounded-2xl border-solid border border-gray-200 bg-surface-elevate-l1">
+            <div className="flex flex-col w-[1200px] p-4 items-start gap-4 rounded-2xl border-solid border border-gray-200 bg-surface-elevate-l1">
                 <div className="flex items-center gap-4">
                     <TextareaWithLimit
                         id=""
@@ -34,33 +40,47 @@ export const GeneratingIdea = () => {
                         onChange={(value) => setKeyword(value)}
                         title="키워드"
                         placeholder="생각나는 키워드를 입력해주세요. (예: 바이브코딩, 도쿄 여행, 가을 메이크업)"
-                        classOfBox="w-[576px] self-stretch"
+                        classOfBox="w-[576px]"
                         classOfTextarea="whitespace-nowrap"
                     />
 
                     <div
-                        className={`flex flex-col w-[576px] p-4 items-start gap-4 rounded-lg bg-surface-elevate-l2 relative border ${
+                        className={`flex flex-col w-[576px] p-4 items-start gap-2 rounded-lg bg-surface-elevate-l2 relative border self-stretch ${
                             isDropdownOpen ? ' border-gray-400' : 'border-transparent'
                         }`}
                     >
                         <div className="font-caption-14m text-gray-600">영상형식</div>
                         <div
-                            className="flex items-start justify-between gap-2 self-stretch select-none "
+                            className="flex items-start justify-between self-stretch select-none "
                             onClick={handleDropdownClick}
                         >
-                            <div className="font-body-16r text-gray-500">영상 형식을 선택해 주세요.</div>
+                            {selectedOption == '' && (
+                                <div className="font-body-16r text-gray-500">영상 형식을 선택해 주세요.</div>
+                            )}
+                            {selectedOption != '' && (
+                                <div className="font-body-16m text-gray-900">{selectedOption}</div>
+                            )}
                             {!isDropdownOpen && <DropdownClose className="cursor-pointer" />}
                             {isDropdownOpen && (
                                 <>
                                     <DropdownOpen className="cursor-pointer" />
                                     <div className="flex flex-col absolute -bottom-45 -left-0">
-                                        <div className="flex w-[576px] p-4 flex-col justify-center items-start gap-2 border border-gray-400 bg-gray-300 rounded-t-lg font-body-16r ">
+                                        <div
+                                            className="flex w-[576px] p-4 flex-col justify-center items-start gap-2 border border-gray-400 hover:bg-gray-200 bg-gray-300 rounded-t-lg font-body-16r"
+                                            onClick={() => handleOptionClick('선택없음')}
+                                        >
                                             선택없음
                                         </div>
-                                        <div className="flex w-[576px] p-4 flex-col justify-center items-start gap-2 border-x border-gray-400 bg-gray-300 font-body-16r">
+                                        <div
+                                            className="flex w-[576px] p-4 flex-col justify-center items-start gap-2 border-x border-gray-400 hover:bg-gray-200 bg-gray-300 font-body-16r"
+                                            onClick={() => handleOptionClick('숏폼 (3분 미만)')}
+                                        >
                                             숏폼 (3분 미만)
                                         </div>
-                                        <div className="flex w-[576px] p-4 flex-col justify-center items-start gap-2 border border-gray-400 bg-gray-300 rounded-b-lg font-body-16r">
+                                        <div
+                                            className="flex w-[576px] p-4 flex-col justify-center items-start gap-2 border border-gray-400 hover:bg-gray-200 bg-gray-300 rounded-b-lg font-body-16r"
+                                            onClick={() => handleOptionClick('롱폼 (3분 이상)')}
+                                        >
                                             롱폼 (3분 이상)
                                         </div>
                                     </div>
@@ -84,7 +104,7 @@ export const GeneratingIdea = () => {
 
                 <button
                     className="flex w-[1168px] h-[48px] px-2 py-4 justify-center items-center gap-2 rounded-2xl 
-                                bg-primary-500 font-body-16b text-gray-900 text-center transition-all active:scale-97"
+                                bg-primary-500 hover:bg-primary-opacity50 font-body-16b text-gray-900 text-center "
                 >
                     콘텐츠 아이디어 생성하기
                 </button>
